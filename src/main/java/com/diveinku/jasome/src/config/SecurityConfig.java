2e5fe4230d657e1d1ebac9e4a6db2f6e0103f379
ggyul <wakegyul@gmail.com>
2022-07-25 18:47:28 +0900

    feat: Apply Swagger for API docs

---
+++ src/main/java/com/diveinku/jasome/src/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.diveinku.jasome.src.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable() // 세션 사용 안하므로
+                // swagger
+                .authorizeRequests()
+                .antMatchers("/swagger-ui/**").permitAll()
+                .antMatchers("/swagger-ui.html").permitAll()
+                .antMatchers("/swagger/**").permitAll()
+                .antMatchers("/swagger-resources/**").permitAll()
+                .antMatchers("/v2/api-docs").permitAll()
+                .antMatchers("/health").permitAll()
+                .and()
+                .authorizeRequests()
+                .anyRequest().authenticated();
+        return http.build();
+    }
+}
