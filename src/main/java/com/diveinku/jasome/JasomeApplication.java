e42fdea1f5ebb158596568a460c9d9218e98e67f
ggyul <wakegyul@gmail.com>
2022-07-25 14:40:43 +0900

    Initial commit

---
+++ src/main/java/com/diveinku/jasome/JasomeApplication.java
@@ -0,0 +1,13 @@
+package com.diveinku.jasome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JasomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diveinku/jasome/JasomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diveinku.jasome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
