006fc419bf435e5289f6a4301f2f999de6686bfd
ggyul <wakegyul@gmail.com>
2022-10-25 19:58:09 +0900

    fix: 인터셉터 주입받는다

--- src/main/java/com/diveinku/jasome/src/config/InterceptorConfig.java
+++ src/main/java/com/diveinku/jasome/src/config/InterceptorConfig.java
@@ -11,28 +11,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class InterceptorConfig implements WebMvcConfigurer {
 
-    private final JwtService jwtService;
     private final AuthenticationInterceptor interceptor;
 
     @Autowired
-    public InterceptorConfig(JwtService jwtService, AuthenticationInterceptor interceptor) {
-        this.jwtService = jwtService;
+    public InterceptorConfig(AuthenticationInterceptor interceptor) {
         this.interceptor = interceptor;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new AuthenticationInterceptor(jwtService))
+        registry.addInterceptor(interceptor)
                 .order(1)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/swagger-ui/**", "/swagger-ui.html", "/swagger/**",
                         "/swagger-resources/**", "/v2/api-docs", "/health");
         WebMvcConfigurer.super.addInterceptors(registry);
     }
-
-    // @Override public void addCorsMappings(CorsRegistry registry) {
-    //     registry.addMapping("/**")
-    //             .allowedOrigins("*")
-    //             .allowedMethods("*");
-    // }
 }
